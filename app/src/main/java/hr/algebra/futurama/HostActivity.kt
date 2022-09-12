2ce19d809a6f78db28574642e89dafb1d42f2d67
Marko Kovačević <mkovac6@racunarstvo.hr>
2022-09-12 16:00:19 +0200

    Step 3

--- app/src/main/java/hr/algebra/futurama/HostActivity.kt
+++ app/src/main/java/hr/algebra/futurama/HostActivity.kt
@@ -1,7 +1,13 @@
 package hr.algebra.futurama
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import hr.algebra.futurama.databinding.ActivityHostBinding
 
 class HostActivity : AppCompatActivity() {
@@ -12,5 +18,58 @@ class HostActivity : AppCompatActivity() {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         binding = ActivityHostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initHamburgerMenu()
+        initNavigation()
+    }
+
+    private fun initHamburgerMenu() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+    }
+
+    private fun initNavigation() {
+        var navController = Navigation.findNavController(this, R.id.navHostFragment)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                toggleDrawer()
+                return true
+            }
+            R.id.menuExit -> {
+                exitApp()
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun exitApp() {
+        AlertDialog.Builder(this).apply {
+            setTitle(R.string.exit)
+            setMessage(getString(R.string.exitConfirm))
+            setIcon(R.drawable.exit)
+            setCancelable(true)
+            setNegativeButton(getString(R.string.cancel), null)
+            setPositiveButton(getString(R.string.ok)) { _, _ -> finish() }
+            show()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawers()
+        } else {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.host_menu, menu)
+        return true
     }
 }
