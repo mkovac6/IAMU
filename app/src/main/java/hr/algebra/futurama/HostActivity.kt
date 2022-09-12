b446208da39490f8d3cf012b32fd1eb406d9da9f
Marko Kovačević <mkovac6@racunarstvo.hr>
2022-09-12 14:59:28 +0200

    Step 1

--- app/src/main/java/hr/algebra/futurama/HostActivity.kt
+++ app/src/main/java/hr/algebra/futurama/HostActivity.kt
@@ -1,7 +1,7 @@
 package hr.algebra.futurama
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import hr.algebra.futurama.databinding.ActivityHostBinding
 
 class HostActivity : AppCompatActivity() {
@@ -9,6 +9,7 @@ class HostActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHostBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         binding = ActivityHostBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
--- app/src/main/java/hr/algebra/futurama/SplashScreenActivity.kt
+++ app/src/main/java/hr/algebra/futurama/SplashScreenActivity.kt
@@ -1,8 +1,16 @@
 package hr.algebra.futurama
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import hr.algebra.futurama.databinding.ActivitySplashScreenBinding
+import hr.algebra.futurama.framework.startActivity
+
+import hr.algebra.futurama.framework.startAnimation
+
+private const val DELAY = 3000L
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -11,5 +19,22 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        startAnimations()
+        redirect()
+    }
+
+    private fun startAnimations() {
+        binding.ivSplash.startAnimation(R.anim.rotate)
+        binding.tvSplash.startAnimation(R.anim.blink)
+    }
+
+    private fun redirect() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity<HostActivity>()
+            }
+            , DELAY
+        )
     }
 }
